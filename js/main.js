f2a93899df1d45cdb0e6288c4b8e596de5dbae5c
Chris Chen <christopher_chen@college.harvard.edu>
2020-10-06 19:01:44 -0700

    Added player dots, nondraggable

--- js/main.js
+++ js/main.js
@@ -6,24 +6,27 @@ let width = 600 - margin.left - margin.right,
 //Separate field dimensions without margins
 let fielddim = 500;
 
+//Set player radius
+let playerRad = 5
+
 //Distance of the foul line to the fence from homeplate, in feet
 // (i.e. home plate to left field wall, along the foul line)
 let outfieldFoulFenceDistance = 325
 // Calculate x and y components of the foul line
-const oFFDy = Math.cos(Math.PI/2) * outfieldFoulFenceDistance
+const oFFDy = Math.cos(Math.PI/4) * outfieldFoulFenceDistance
 
 //Distance between bases (e.g. home to first)
 let baseDistance = 90
 // Calculate x and y components of the distance
-const bDy = Math.cos(Math.PI/2) * baseDistance
+const bDy = Math.cos(Math.PI/4) * baseDistance
 
 let yfieldLineScale = d3.scaleLinear()
     .domain([0,oFFDy])
     .range([450,200])
 
 let xfieldLineScale = d3.scaleLinear()
-    .domain([0,oFFDy])
-    .range([0,250])
+    .domain([-oFFDy,oFFDy])
+    .range([-250,250])
 
 drawField();
 drawPlayers("data/players.csv");
@@ -40,7 +43,7 @@ function drawField(){
         .attr("stroke","black")
         .attr("fill","white")
         .attr("d","M 250 450 L " +
-            (fielddim/2 - xfieldLineScale(oFFDy)) + " " + yfieldLineScale(oFFDy) +
+            (fielddim/2 + xfieldLineScale(-oFFDy)) + " " + yfieldLineScale(oFFDy) +
             " Q 100 50 250 50 Q 400 50 " +
             (xfieldLineScale(oFFDy) + fielddim/2) + " " + yfieldLineScale(oFFDy) +
             " Z");
@@ -50,7 +53,7 @@ function drawField(){
         .attr("stroke","black")
         .attr("fill","white")
         .attr("d","M 250 450 L " +
-            (fielddim/2 - xfieldLineScale(bDy)) + " " + yfieldLineScale(bDy) +
+            (fielddim/2 + xfieldLineScale(-bDy)) + " " + yfieldLineScale(bDy) +
             " L " + (fielddim/2) + " " + (yfieldLineScale(2 * bDy)) + " L " +
             (xfieldLineScale(bDy) + fielddim/2) + " " + yfieldLineScale(bDy) +
             " Z");
@@ -66,15 +69,55 @@ function drawPlayers(inputPlayers){
         return row;
     })
         .then(data => {
+
             let players = d3.select("#field").select("svg")
                 .append("g")
-                .attr("id","players");
+                .attr("id","players")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
             players.selectAll("circle")
                 .data(data)
                 .enter()
-                .append("circle");/*
-                .attr("x")
-*/
+                .append("circle")
+                .attr("id",d => d.position)
+                .attr("r",playerRad)
+                .attr("cx",d => {
+                    return xfieldLineScale(positionSVGCoords(d.position,"x")) + fielddim/2
+                })
+                .attr("cy",d => {
+                    return yfieldLineScale(positionSVGCoords(d.position,"y"))
+                })
+
         })
+}
+
+// Input: Field position of player (e.g. 3B, SS)
+// Output: x or y SVG coordinate of the player on the pitch
+function positionSVGCoords(position,dimension){
+    if (dimension === "x"){
+        if (position === "P") {return 0}
+        else if (position === "C") {return 0}
+        else if (position === "1B") {return 55}
+        else if (position === "2B") {return 10}
+        else if (position === "3B") {return -55}
+        else if (position === "SS") {return -25}
+        else if (position === "RF") {return 150}
+        else if (position === "LF") {return -150}
+        else if (position === "CF") {return 0}
+
+        else {return 50;}
+    }
+    else {
+        if (position === "P") {return 60.5;}
+        else if (position === "C") {return 0}
+        else if (position === "1B") {return 65}
+        else if (position === "2B") {return 120}
+        else if (position === "3B") {return 95}
+        else if (position === "SS") {return 120}
+        else if (position === "RF") {return 225}
+        else if (position === "LF") {return 225}
+        else if (position === "CF") {return 300}
+        else {return 0;}
+    }
+
 }
